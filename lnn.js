1fd1e18962286af53770f61fc691a8800f4a8424
Anderson <MDAnderson@samaritan.org>
2019-05-08 11:11:13 -0400

    first commit

--- lnn.js
+++ lnn.js
@@ -1,6 +1,36 @@
 
 
+var MAX_WEIGHT = 1, MIN_WEIGHT = -1;
 
-var axons = [
-    
+var net = [
+    {
+        output: rand(MAX_WEIGHT, MIN_WEIGHT),
+        axons: [1, 2, 4]
+    },
+    {
+        output: rand(MAX_WEIGHT, MIN_WEIGHT),
+        axons: [2, 3, 4, 5]
+    },
+    {
+        output: rand(MAX_WEIGHT, MIN_WEIGHT),
+        axons: [3, 4]
+    },
+    {
+        output: rand(MAX_WEIGHT, MIN_WEIGHT),
+        axons: [5]
+    },
+    {
+        output: rand(MAX_WEIGHT, MIN_WEIGHT)
+    },
+    {
+        output: rand(MAX_WEIGHT, MIN_WEIGHT)
+    },
 ];
+
+function rand(max, min) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function init() {
+    console.log(net);
+}
